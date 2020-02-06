c8bb52fbc24d31deaad626447307b3090f722278
LIDIA GOMES CUNHA <19267272@CFP127ALUNO.LOCAL>
2020-02-06 11:25:59 -0300

    Refatorando e utilizando arrow function

--- AULA1/script1.js
+++ AULA1/script1.js
@@ -1,28 +1,49 @@
 const $calcular = document.getElementById('calcular')
 
-function calcularMedia () {
+const calcularMedia = (n1, n2) => (parseInt(n1) + parseInt(n2)) / 2
+
+
+const verificaSituacao = (media) => media >= 5 ? "Aprovado" : "Reprovado";
+
+
+const exibirMedia = () => {
+
     const $nome = document.getElementById('nome');
-    const $nota1 = document.getElementById('nota1');
-    const $nota2 = document.getElementById('nota2');
+    const nota1 = document.getElementById('nota1').value;
+    const nota2 = document.getElementById('nota2').value;
     const $media = document.getElementById('media');
     const $situacao = document.getElementById('situacao');
+    const $conceito = document.getElementById('conceito');
 
-     const media = (parseInt($nota1.value) + parseInt($nota2.value)) / 2;
-    console.log($nome.value);    
-
-    if (media >= 5){
-        $situacao.value = "Aprovado";
-        $situacao.classList.remove('reprovado');
-        $situacao.classList.add('aprovado');
-        $situacao.style.color = 'green';
-    } else {
-        $situacao.value = "Reprovado";
-        $situacao.classList.remove('aprovado');
-        $situacao.classList.add('reprovado');
-        $situacao.style.color = 'red'; 
-    }
-
+    const media = calcularMedia (nota1, nota2);
     $media.value = media;
+    
+    $conceito.value = conceito(media);
+    $situacao.value = verificaSituacao (media);
 
 }
-$calcular.addEventListener('click', calcularMedia);
+
+$calcular.addEventListener('click', exibirMedia);
+const calcular2 = () => {
+
+}
+
+const conceito = (media) => {
+    if (media < 3){
+        return "E"
+    } else {
+        if (media < 5){
+            return "D"
+        } else {
+            if (media < 8){
+                return "C"
+            } else {
+                if (media < 10){
+                    return "B"
+                } else {
+                    return "A"
+                }
+            }
+        }
+    }    
+}
